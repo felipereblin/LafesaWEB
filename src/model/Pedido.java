2eb63ad1dce32ebf520422487936b5bdf6d83321
felipereblin <felipereblin7@gmail.com>
2018-11-06 20:50:24 -0200

    comit construtor pedido

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -7,6 +7,13 @@ public class Pedido {
 	private int id;
 	private List<Item> itens;
 	private double valorTotal;
+	
+	public Pedido(int id, List<Item> itens, double valorTotal) {
+		super();
+		this.id = id;
+		this.itens = itens;
+		this.valorTotal = valorTotal;
+	}
 
 	public int getId() {
 		return id;
