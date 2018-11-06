f05f91dee7faee0e20c57b135af222da582711f6
larissacensi <larissacensi3@gmail.com>
2018-11-06 20:00:53 -0200

    commit criação model Produto

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -1,5 +1,35 @@
 package model;
 
 public class Produto {
-
+	private int id;
+	private String nome;
+	private Marca marca;
+	private double precoUnitario;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Marca getMarca() {
+		return marca;
+	}
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+	public double getPrecoUnitario() {
+		return precoUnitario;
+	}
+	public void setPrecoUnitario(double precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+	
+	
 }
