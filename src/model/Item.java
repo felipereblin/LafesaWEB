05225891c0c757eb93b3bcd990f2a74a526e1a12
sabrinadiehl1 <sabrinadiehl1@users.noreply.github.com>
2018-11-06 20:00:31 -0200

    Commit criação classe item.

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,5 +1,41 @@
 package model;
 
 public class Item {
-
+	
+	private int id;
+	private Pedido pedido; 
+	private Produto produto; 
+	private int quantidade;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Item(int id, Pedido pedido, Produto produto, int quantidade) {
+		super();
+		this.id = id;
+		this.pedido = pedido;
+		this.produto = produto;
+		this.quantidade = quantidade;
+	}
+	
 }
