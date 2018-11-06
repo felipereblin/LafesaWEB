4bb7d9de5460a9a8172172ee1626d617ee4e1547
sabrinadiehl1 <sabrinadiehl1@users.noreply.github.com>
2018-11-06 21:00:05 -0200

    Criação classe dao

---
+++ src/dao/ItemDao.java
@@ -0,0 +1,197 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Cliente;
+import model.Item;
+import model.Pedido;
+
+
+public class ItemDao implements Dao<Item>{
+	
+	private static final String GET_BY_ID = "SELECT * FROM item NATURAL JOIN cliente WHERE id = ?";
+	private static final String GET_ALL = "SELECT * FROM item NATURAL JOIN cliente";
+	private static final String INSERT = "INSERT INTO item (pedido_id, produto_id, quantidade) "	+ "VALUES (?, ?, ?)";
+	private static final String UPDATE = "UPDATE item SET pedido_id = ?, produto_id = ?, quantidade = ? WHERE id = ?";
+	private static final String DELETE = "DELETE FROM item WHERE id = ?";
+	
+	public ItemDao() {
+		try {
+			createTable();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void createTable() throws SQLException {
+	    final String sqlCreate = "CREATE TABLE IF NOT EXISTS item"
+	            + "  (id           INTEGER,"
+	            + "   pedido_id      INTEGER,"
+	            + "   produto_id   INTEGER,"
+	            + "   quantidade	   INTEGER,"
+	            + "   FOREIGN KEY (pedido_id) REFERENCES pedido(id),"
+	            + "   FOREIGN KEY (produto_id) REFERENCES produto(id),"
+	            + "   PRIMARY KEY (id))";
+	    
+	    Connection conn = DbConnection.getConnection();
+
+	    Statement stmt = conn.createStatement();
+	    stmt.execute(sqlCreate);
+	} 
+	
+	
+	private Item getItemFromRS(ResultSet rs) throws SQLException
+    {
+		Item item = new Item();
+			
+		item.setId( rs.getInt("id") );
+		item.setCliente( new Pedido(rs.getArray(columnIndex)) );
+		item.setId( rs.getInt("quantidade") );
+	
+		return item;
+    }
+	
+	@Override
+	public Item getByKey(int id) {
+		Connection conn = DbConnection.getConnection();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		Item item = null;
+		
+		try {
+			stmt = conn.prepareStatement(GET_BY_ID);
+			stmt.setInt(1, id);
+			rs = stmt.executeQuery();
+			
+			if (rs.next()) {
+				item = getItemFromRS(rs);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(conn, stmt, rs);
+		}
+		
+		return item;
+	}
+
+	@Override
+	public List<Item> getAll() {
+		Connection conn = DbConnection.getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		List<Item> item = new ArrayList<>();
+		
+		try {
+			stmt = conn.createStatement();
+			
+			rs = stmt.executeQuery(GET_ALL);
+			
+			while (rs.next()) {
+				item.add(getItemFromRS(rs));
+			}			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(conn, stmt, rs);
+		}
+		
+		return item;
+	}
+
+	@Override
+	public void insert(Item item) {
+		Connection conn = DbConnection.getConnection();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			stmt = conn.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1, item.getPedido().getId());
+			stmt.setInt(2, item.getProduto().getId());
+			stmt.setInt(3, item.getQuantidade());
+
+			stmt.executeUpdate();
+			rs = stmt.getGeneratedKeys();
+			
+			if (rs.next()) {
+				item.setId(rs.getInt(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(conn, stmt, rs);
+		}
+
+	}
+
+	@Override
+	public void delete(int id) {
+		Connection conn = DbConnection.getConnection();
+		
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = conn.prepareStatement(DELETE);
+			
+			stmt.setInt(1, id);
+			
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(conn, stmt, null);
+		}
+	}
+
+	@Override
+	public void update(Item item) {
+		Connection conn = DbConnection.getConnection();
+		
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = conn.prepareStatement(UPDATE);
+			stmt.setInt(1, item.getPedido().getId());
+			stmt.setInt(2, item.getProduto().getId());
+			stmt.setInt(3, item.getQuantidade());
+			stmt.setInt(4, item.getId());
+			
+			stmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close (conn, stmt, null);
+		}
+	}
+	
+	private static void close(Connection myConn, Statement myStmt, ResultSet myRs) {
+		try {
+			if (myRs != null) {
+				myRs.close();
+			}
+			
+			if (myStmt != null) {
+				myStmt.close();
+			}
+			
+			if (myConn != null) {
+				myConn.close();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao fechar recursos.", e);
+		}
+		
+	}
+	
+}
---
+++ src/dao/MarcaDao.java
@@ -0,0 +1,188 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import model.Marca;
+
+
+
+public class MarcaDao implements Dao<Marca> {
+	
+	private static final String GET_BY_ID = "SELECT * FROM marca WHERE id = ?";
+	private static final String GET_ALL = "SELECT * FROM marca";
+	private static final String INSERT = "INSERT INTO marca (nome) "+ "VALUES (?)";
+	private static final String UPDATE = "UPDATE marca SET nome = ?";
+	private static final String DELETE = "DELETE FROM marca WHERE id = ?";
+	
+	public MarcaDao() {
+		try {
+			createTable();
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao criar tabela no banco.", e);
+			//e.printStackTrace();
+		} 
+	}
+	
+	private void createTable() throws SQLException {
+	    String sqlCreate = "CREATE TABLE IF NOT EXISTS marca"
+	            + "  (id           INTEGER,"
+	            + "   nome            VARCHAR(50),"
+	            + "   PRIMARY KEY (id))";
+	    
+	    Connection conn = DbConnection.getConnection();
+
+
+	    Statement stmt = conn.createStatement();
+	    stmt.execute(sqlCreate);
+	    
+	    close(conn, stmt, null);
+	}
+	
+	
+	private Marca getMarcaFromRS(ResultSet rs) throws SQLException
+    {
+		Marca marca = new Marca();
+			
+		marca.setId( rs.getInt("id") );
+		marca.setNome( rs.getString("nome"));
+	
+		return marca;
+    }
+	
+	@Override
+	public Marca getByKey(int id) {
+		Connection conn = DbConnection.getConnection();
+		
+		Marca marca = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			stmt = conn.prepareStatement(GET_BY_ID);
+			stmt.setInt(1, id);
+			rs = stmt.executeQuery();
+			
+			if (rs.next()) {
+				marca = getMarcaFromRS(rs);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao obter marca pela chave.", e);
+		} finally {
+			close(conn, stmt, rs);
+		}
+		
+		return marca;
+	}
+
+	@Override
+	public List<Marca> getAll() {
+		Connection conn = DbConnection.getConnection();
+		
+		List<Marca> marcas = new ArrayList<>();
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			stmt = conn.createStatement();
+			
+			rs = stmt.executeQuery(GET_ALL);
+			
+			while (rs.next()) {
+				marcas.add(getMarcaFromRS(rs));
+			}			
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao obter todos os marcas.", e);
+		} finally {
+			close(conn, stmt, rs);
+		}
+		
+		return marcas;
+	}
+
+	@Override
+	public void insert(Marca marca) {
+		Connection conn = DbConnection.getConnection();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			stmt = conn.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+			stmt.setString(1, marca.getNome());
+			
+			stmt.executeUpdate();
+			rs = stmt.getGeneratedKeys();
+			
+			if (rs.next()) {
+				marca.setId(rs.getInt(1));
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao inserir marca.", e);
+		}finally {
+			close(conn, stmt, rs);
+		}
+
+	}
+
+	@Override
+	public void delete(int id) {
+		Connection conn = DbConnection.getConnection();
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = conn.prepareStatement(DELETE);
+			
+			stmt.setInt(1, id);
+			
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao remover marca.", e);
+		} finally {
+			close(conn, stmt, null);
+		}
+	}
+
+	@Override
+	public void update(Marca marca) {
+		Connection conn = DbConnection.getConnection();
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = conn.prepareStatement(UPDATE);
+			stmt.setString(1, marca.getNome());
+			stmt.setInt(2, marca.getId());
+			
+			stmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao atualizar marca.", e);
+		} finally {
+			close(conn, stmt, null);
+		}
+	}
+	
+	private static void close(Connection myConn, Statement myStmt, ResultSet myRs) {
+		try {
+			if (myRs != null) {
+				myRs.close();
+			}
+			
+			if (myStmt != null) {
+				myStmt.close();
+			}
+			
+			if (myConn != null) {
+				myConn.close();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao fechar recursos.", e);
+		}
+		
+	}
+
+}
